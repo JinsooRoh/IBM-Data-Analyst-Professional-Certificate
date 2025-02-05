0899729907c6c97aa4a913239482fd75c58d2ea4
JINSOO ROH <js09070@gmail.com>
2025-02-05 22:40:59 +0900

    Initial commit with all files

---
+++ Python for Data Science, AI & Development/Hands-on Lab 1.py
@@ -0,0 +1,27 @@
+
+
+
+# 1. Say 'Hello' to the world in Python
+print('Hello, Python!')
+
+# 2. What version of Python are we using?
+# import sys
+# print(sys.version)
+
+# 3. Writing comments in Python
+print('Hello, Python!') # This line prints a string
+# print("Hi")
+
+# 4. Errors in Python
+# frint("Hello, Python!") = NameError
+#print("Hello, Python!) = SyntaxError
+
+# 5. Does Python know about your error before it runs your code?
+print("This will be printed")
+print("This will cause an error")
+print("This will NOT be printed")
+
+
+# 6. Exercise: Your First Program
+z = 2 + 15
+print(z)
