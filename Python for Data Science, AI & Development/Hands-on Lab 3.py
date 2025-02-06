8427e59a2d6a97e75e2752b243503bd96c8ebfd4
JINSOO ROH <js09070@gmail.com>
2025-02-06 15:42:42 +0900

    Hands-on Lab 3

---
+++ Python for Data Science, AI & Development/Hands-on Lab 3.py
@@ -0,0 +1,58 @@
+# 1. Expressions
+# Addition operation expression
+43 + 60 + 16 + 41
+
+# Subtraction operation expression
+50 - 60
+
+# Multiplication operation expression
+5 * 5
+
+# Division operation expression
+25 / 5
+25 / 6
+160//60 
+
+# Mathematical expression
+30 + 2 * 60
+
+# Mathematical expression
+(30 + 2) * 60
+
+# 2. Variables
+# Store value into variable
+x = 43 + 60 + 16 + 41
+print(x)
+
+# Overwrite variable with new value
+y = x / 60
+print(y)
+
+# Name the variables meaningfully
+total_min = 43 + 42 + 57 # Total length of albums in minutes
+print(total_min)
+
+# Name the variables meaningfully
+total_hours = total_min / 60 # Total length of albums in hours 
+print(total_hours)
+
+# ComplicateD expression
+total_hours = (43 + 42 + 57) // 60  # Total hours in a single expression
+print(total_hours)
+
+# 3. Exercise: Expressions
+30 + 20 - 40
+
+(55 - 5) / 10 
+
+6 * 10 / 12
+
+# 4. Exercise: Variables in Python
+x = 3 + 2 * 2
+print(x)
+
+y = (3 + 2) * 2
+print(y)
+
+z = x + y
+print(z)
